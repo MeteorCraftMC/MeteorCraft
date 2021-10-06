ed37dccd31f2fc5d87d534ce54c177e585b630d6
myoun <aiden080605@gmail.com>
2021-10-06 22:00:35 +0900

    * 2021/10/06 10:00 [+] Can Charge Tank [+] Can See Energy

--- src/main/java/com/github/meteorcraft/trinkets/OxygenTankItem.java
+++ src/main/java/com/github/meteorcraft/trinkets/OxygenTankItem.java
@@ -3,11 +3,23 @@ package com.github.meteorcraft.trinkets;
 import com.github.meteorcraft.MeteorWorlds;
 import dev.emi.trinkets.api.SlotReference;
 import dev.emi.trinkets.api.TrinketItem;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.ItemStack;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
 import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+import team.reborn.energy.api.base.SimpleBatteryItem;
 
-public class OxygenTankItem extends TrinketItem {
+import java.util.List;
+
+public class OxygenTankItem extends TrinketItem implements SimpleBatteryItem {
+
+    public final long capacity = 5000; // Need to change
+    public final long maxInput = 50; // Need to change
+    public final long maxOutput = 50; // Need to change
+
     public OxygenTankItem(Settings settings) {
         super(settings);
     }
@@ -20,5 +32,25 @@ public class OxygenTankItem extends TrinketItem {
                 });
             }
         }
+    }
+
+    @Override
+    public long getEnergyCapacity() {
+        return capacity;
+    }
+
+    @Override
+    public long getEnergyMaxInput() {
+        return maxInput;
+    }
+
+    @Override
+    public long getEnergyMaxOutput() {
+        return maxOutput;
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        tooltip.add(new LiteralText("Energy : "+ getStoredEnergy(stack)));
     }
 }
